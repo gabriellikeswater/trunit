3dc804642b81e45af490bd0f6bd61e8d636006fa
Gabriel Likes Water <90223564+gabriellikeswater@users.noreply.github.com>
2023-07-16 20:49:35 +0100

    Add files via upload

---
+++ trunit-dev.bat
@@ -0,0 +1,22 @@
+@echo off
+title ARE YOU DREAMING?
+
+:mainarea
+
+cls
+
+echo                       ______
+echo                      /\__  _\
+echo                      \/_/\ \/
+echo                         \ \_\
+echo                          \/_/
+echo    ______     __  __     __   __        __     ______
+echo   /\  == \   /\ \/\ \   /\ "-.\ \      /\ \   /\__  _\
+echo   \ \  __-   \ \ \_\ \  \ \ \-.  \     \ \ \  \/_/\ \/
+echo    \ \_\ \_\  \ \_____\  \ \_\\"\_\     \ \_\    \ \_\
+echo     \/_/ /_/   \/_____/   \/_/ \/_/      \/_/     \/_/
+echo yo
+echo.
+
+
+pause
