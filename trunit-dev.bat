4624442fa9ebef5eb11e42df38fc9f06d67c50c5
Gabriel Likes Water <90223564+gabriellikeswater@users.noreply.github.com>
2023-09-11 20:06:21 +0100

    Major improvements!
    - optimized mainarea
    - info
    - optimized user input
    - runarea

--- trunit-dev.bat
+++ trunit-dev.bat
@@ -16,31 +16,77 @@ echo   \ \  __-   \ \ \_\ \  \ \ \-.  \     \ \ \  \/_/\ \/
 echo    \ \_\ \_\  \ \_____\  \ \_\\"\_\     \ \_\    \ \_\
 echo     \/_/ /_/   \/_____/   \/_/ \/_/      \/_/     \/_/
 echo.
-echo  - To run all the stuff needed for the server, Type "runall"
-echo  - To run only the local server, Type "run"
-echo  - To run only the playit proxy, Type "runproxy"
-echo  - To run only the discord bot, Type "runbot"
+echo  - Type "runarea" to run this server.
 echo.
 echo  -----------------------Notes(below)---------------------
 echo.
-echo  - If you don't have a discord bot or playit proxy,
-echo    when using "runall", you might get errors,
-echo    it's OK. The server will still work :D
-echo  - While running, type "exit" to save and stop the server.
 echo  - While running, type "help" for a list of all commands.
+echo  - While running, type "exit" to save and stop the server.
 echo  - You can't close all the windows with the "exit"
 echo    command so you have to manually close them :P
+echo  - Anything typed in that's not a command in the program
+echo    might close the program.
 echo.
 
-set /p option1= [%username%@runit]~ 
+set /p option1= [%username%]~ 
 if %option1% == mainarea goto mainarea
+if %option1% == runarea goto runarea
+if %option1% == info goto info
 if %option1% == runall goto runall
 if %option1% == run goto run
 if %option1% == runproxy goto runproxy
 if %option1% == runbot goto runbot
 
 goto mainarea
+pause
 
+:runarea
+cls
+
+echo.
+echo      TYPE "mainarea" TO GO BACK TO THE MAIN AREA.
+echo.
+echo  - To run all the stuff needed for the server, Type "runall"
+echo    (If you don't have a discord bot or playit proxy
+echo    you might get errors, it's OK. The server will
+echo    still work :D )
+echo  - To run only the local server, Type "run"
+echo  - To run only the playit proxy, Type "runproxy"
+echo  - To run only the discord bot, Type "runbot"
+echo.
+
+set /p option1= [%username%]~ 
+if %option1% == mainarea goto mainarea
+if %option1% == runarea goto runarea
+if %option1% == info goto info
+if %option1% == runall goto runall
+if %option1% == run goto run
+if %option1% == runproxy goto runproxy
+if %option1% == runbot goto runbot
+
+goto mainarea
+pause
+
+:info
+cls
+
+echo    TYPE "mainarea" TO GO BACK TO THE MAIN AREA.
+echo.
+echo.
+echo  - https://github.com/gabriellikeswater/runit
+echo.
+echo.
+
+set /p option1= [%username%]~ 
+if %option1% == mainarea goto mainarea
+if %option1% == runarea goto runarea
+if %option1% == info goto info
+if %option1% == runall goto runall
+if %option1% == run goto run
+if %option1% == runproxy goto runproxy
+if %option1% == runbot goto runbot
+
+goto mainarea
 pause
 
 :runall
@@ -62,15 +108,16 @@ echo.
 echo   TYPE "mainarea" TO GO BACK TO THE MAIN AREA.
 echo.
 
-set /p option1= [%username%@runit]~ 
+set /p option1= [%username%]~ 
 if %option1% == mainarea goto mainarea
+if %option1% == runarea goto runarea
+if %option1% == info goto info
 if %option1% == runall goto runall
 if %option1% == run goto run
 if %option1% == runproxy goto runproxy
 if %option1% == runbot goto runbot
 
 goto mainarea
-
 pause
 
 :run
@@ -86,15 +133,16 @@ echo.
 echo   TYPE "mainarea" TO GO BACK TO THE MAIN AREA.
 echo.
 
-set /p option1= [%username%@runit]~ 
+set /p option1= [%username%]~ 
 if %option1% == mainarea goto mainarea
+if %option1% == runarea goto runarea
+if %option1% == info goto info
 if %option1% == runall goto runall
 if %option1% == run goto run
 if %option1% == runproxy goto runproxy
 if %option1% == runbot goto runbot
 
 goto mainarea
-
 pause
 
 :runproxy
@@ -107,15 +155,16 @@ echo.
 echo   TYPE "mainarea" TO GO BACK TO THE MAIN AREA.
 echo.
 
-set /p option1= [%username%@runit]~ 
+set /p option1= [%username%]~ 
 if %option1% == mainarea goto mainarea
+if %option1% == runarea goto runarea
+if %option1% == info goto info
 if %option1% == runall goto runall
 if %option1% == run goto run
 if %option1% == runproxy goto runproxy
 if %option1% == runbot goto runbot
 
 goto mainarea
-
 pause
 
 :runbot
@@ -129,13 +178,14 @@ echo.
 echo   TYPE "mainarea" TO GO BACK TO THE MAIN AREA.
 echo.
 
-set /p option1= [%username%@runit]~ 
+set /p option1= [%username%]~ 
 if %option1% == mainarea goto mainarea
+if %option1% == runarea goto runarea
+if %option1% == info goto info
 if %option1% == runall goto runall
 if %option1% == run goto run
 if %option1% == runproxy goto runproxy
 if %option1% == runbot goto runbot
 
 goto mainarea
-
 pause
